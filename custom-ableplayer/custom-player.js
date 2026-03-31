5c99f34547d30f00213adf7a1931d60041df45d1
Joan Galtés i Moreno <joan.galtes@tothomweb.com>
2026-03-31 18:07:01 +0200

    Override the path where AblePlayer should look up to find the translation files.

--- custom-ableplayer/custom-player.js
+++ custom-ableplayer/custom-player.js
@@ -722,17 +722,64 @@
 (function ($) {
   AblePlayer.prototype.getSupportedLangs = function () {
     // Returns an array of languages for which AblePlayer has translation tables        
-    var langs = ['ca', 'cs', 'da', 'de', 'en', 'es', 'fr', 'he', 'id', 'it', 'ja', 'ms', 'nb', 'nl', 'oc-aranes', 'pl', 'pt', 'pt-br', 'sv', 'tr', 'zh-tw'];
+    var langs = ['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv', 'ny', 'oc', 'oc-aranes', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'];
     return langs;
   };
 })(jQuery);
 
+// (function ($) {
+//   var isoLangs = {
+//     "oc-aranes": {
+//       "name": "Aranese",
+//       "nativeName": "Aranese"
+//     },
+//   }
+// })(jQuery);
+
+/* ═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+   FORCE TRANSLATION FILES TO LOAD FROM /TRANSLATIONS
+════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════ */
 (function ($) {
-  var isoLangs = {
-    "oc-aranes": {
-      "name": "Aranese",
-      "nativeName": "Aranese"
-    },
+  if (!window.AblePlayer || !AblePlayer.prototype) return;
+
+  var originalGetTranslationText = AblePlayer.prototype.getTranslationText;
+  if (typeof originalGetTranslationText === 'function') {
+    AblePlayer.prototype.getTranslationText = function () {
+      var thisObj = this;
+      var originalRootPath = thisObj.rootPath;
+
+      // Force translation lookup to /translations/<lang>.json
+      thisObj.rootPath = '/';
+
+      var result = originalGetTranslationText.apply(thisObj, arguments);
+
+      var restoreRootPath = function () {
+        thisObj.rootPath = originalRootPath;
+      };
+
+      if (result && typeof result.always === 'function') {
+        result.always(restoreRootPath);
+      } else {
+        restoreRootPath();
+      }
+
+      return result;
+    };
+  }
+
+  var originalGetSampleDescriptionText = AblePlayer.prototype.getSampleDescriptionText;
+  if (typeof originalGetSampleDescriptionText === 'function') {
+    AblePlayer.prototype.getSampleDescriptionText = function () {
+      var thisObj = this;
+      var originalRootPath = thisObj.rootPath;
+
+      // Keep sample description translations aligned with /translations
+      thisObj.rootPath = '/';
+
+      var result = originalGetSampleDescriptionText.apply(thisObj, arguments);
+      thisObj.rootPath = originalRootPath;
+      return result;
+    };
   }
 })(jQuery);
 
