1a2591252eefc7612ec73a2c5fb8f29b04037101
Joan Galtés i Moreno <joan.galtes@tothomweb.com>
2026-03-31 21:29:25 +0200

    Updated array of languages and some trnaslation mistakes.

--- custom-ableplayer/custom-player.js
+++ custom-ableplayer/custom-player.js
@@ -722,7 +722,7 @@
 (function ($) {
   AblePlayer.prototype.getSupportedLangs = function () {
     // Returns an array of languages for which AblePlayer has translation tables        
-    var langs = ['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv', 'ny', 'oc', 'oc-aranes', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'];
+    var langs = ['ab', 'am', 'ar', 'as', 'bh', 'bg', 'ca', 'cs', 'cu', 'da', 'de', 'dv', 'el', 'en', 'es', 'eu', 'fr', 'gl', 'gn', 'gu', 'hr', 'hu', 'it', 'iu', 'ja', 'km', 'kn', 'kv', 'mi', 'nb', 'nl', 'nn', 'no', 'oc', 'oc-aranes', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'sr', 'sv', 'te', 'ti', 'uk', 'yi', 'zh'];
     return langs;
   };
 })(jQuery);
